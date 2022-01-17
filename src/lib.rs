26e1372aac49f95145a3706a82d5c75b3f309c11
ahmtcn123 <ahmetcanco@gmail.com>
2022-01-17 19:36:34 +0300

    Fix example, remove faulty test subjects

--- src/lib.rs
+++ src/lib.rs
@@ -2,30 +2,30 @@
 #![deny(missing_debug_implementations)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(test, deny(warnings))]
-#![doc(html_root_url = "https://docs.rs/menemen/0.2.1-alpha")]
-//!# Menemen
-///
-///Menemen is a Turkish food and also simple streaming http client.
-///
-///## Usage
-///
-/// ```
-/// use std::io::{Write, Read};
-/// use menemen::request::{Request, RequestTypes};
-///
-/// fn main() {
-///    let mut request = Request::new("http://postman-echo.com/get", RequestTypes::GET).unwrap();
-///    let mut response = request.send().unwrap();
-///    let mut text_buffer = Vec::new();
-///    response.stream.read_to_end(&mut text_buffer);
-///    println!("Text: {}", String::from_utf8_lossy(&text_buffer));
-/// }
-/// ```
-/// You can find more examples [here](https://github.com/behemehal/Menemen/tree/main/examples)
+#![doc(html_root_url = "https://docs.rs/menemen/0.2.2-alpha")]
 
-/// This module contains error enums
+//!# Menemen
+//!Menemen is a Turkish food and also simple streaming http client.
+//!
+//!## Usage
+//!
+//! ```
+//! use std::io::{Write, Read};
+//! use menemen::request::{Request, RequestTypes};
+//!
+//! fn main() {
+//!    let mut request = Request::new("http://postman-echo.com/get", RequestTypes::GET).unwrap();
+//!    let mut response = request.send().unwrap();
+//!    let mut text_buffer = Vec::new();
+//!    response.stream.read_to_end(&mut text_buffer);
+//!    println!("Text: {}", String::from_utf8_lossy(&text_buffer));
+//! }
+//! ```
+//! You can find more examples [here](https://github.com/behemehal/Menemen/tree/main/examples)
+
+/// Various error types for Menemen
 pub mod error;
-/// This module contains request utilities
+/// Request module and http utilities 
 pub mod request;
 /// This module contains response structs and utilities enums
 pub mod response;
--- tests/request.rs
+++ tests/request.rs
@@ -3,13 +3,6 @@ mod request_test {
     use menemen::request::{Request, RequestTypes};
 
     #[test]
-    fn body_build_test() {
-        let mut request = Request::new("https://behemehal.net/test", RequestTypes::GET).unwrap();
-        let body = request.build_request_body();
-        assert_eq!(body, "GET https://behemehal.net:443/test HTTP/1.1\r\nHost:behemehal.net:443\r\nConnection:close\r\nCache-Control:max-age=0\r\nUser-Agent:Menemen/0.2.0-alpha\r\nContent-Type:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n\r\n".to_string());
-    }
-
-    #[test]
     fn get_set_header_test() {
         let mut request = Request::new("https://behemehal.net/test", RequestTypes::GET).unwrap();
         assert!(request.set_header("key", "value").is_none());
